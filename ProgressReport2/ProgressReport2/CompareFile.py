6c5ccfd71c40a0fde43046dc2a984289b42405de
TainedeBaes <78296222+TainedeBlaze@users.noreply.github.com>
2021-10-25 22:41:33 +0200

    Delete ProgressReport2/ProgressReport2 directory

--- ProgressReport2/ProgressReport2/CompareFile.py
+++
@@ -1,7 +0,0 @@
-from difflib import Differ
-  
-with open('IMU-data-2021-10-25-19:35:45.txt') as file_1, open('decompressedData_of_encrypted_fileofIMU.txt.txt') as file_2:
-    differ = Differ()
-  
-    for line in differ.compare(file_1.readlines(), file_2.readlines()):
-        print(line)
--- ProgressReport2/ProgressReport2/Testing.py
+++
@@ -1,6 +0,0 @@
-import glob
-import os 
-  
-#os.system('python3 SendData.py ' + "IMU-data-2021-10-25-20:38:19.csv" )
-
-os.system('python3 RecieveData.py' + " encrypted_fileofIMU.txt")
